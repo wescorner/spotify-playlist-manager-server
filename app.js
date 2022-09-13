6d8e0c9b9da501d287948055669e331125b811d7
Wesley Corner <wescorner@gmail.com>
2022-09-13 16:56:18 -0400

    no www

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(cors({ origin: "https://www.spotifyplaylistmanager.com", credentials: true }));
+app.use(cors({ origin: "https://spotifyplaylistmanager.com", credentials: true }));
 app.use(cookieParser("aasdfads", { sameSite: "none", domain: "spotifyplaylistmanager.com" }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
